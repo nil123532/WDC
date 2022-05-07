a48da4f0d07afca3ec6f254b56e4e2a0ae781c4d
Patrick Sumarli <70998845+PatrickS136@users.noreply.github.com>
2022-05-07 11:48:30 +0000

    Sidebar

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,8 +1,12 @@
 var vueinst = new Vue({
     el : "#app",
     data : {
-        test : "Test"
+        currentPage : -1,
+        pages : [{name : "Home", link : "#"}, {name : "Events", link : "#"}, {name : "Settings", link : "#"}]
     },
     methods : {
-    }
+        changePage : i => {
+            this.currentPage = i;
+        }
+    },
 });
