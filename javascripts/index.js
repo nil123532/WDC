9e83198692d0e036c03de1fda92274d92de579d0
Patrick Sumarli <70998845+PatrickS136@users.noreply.github.com>
2022-05-08 06:56:11 +0000

    Anonymous event linked

--- javascripts/index.js
+++ javascripts/index.js
@@ -24,7 +24,8 @@ var vueinst = new Vue({
             time : "xd",
             count : 100,
             img : "https://www.w3schools.com/w3css/img_lights.jpg"
-        },]
+        },],
+        dummyAvailability : ["December 9th 2022, 9 a.m", "December 13th 2022, 2:30 p.m. - 3:00 p.m.", "December 13th 2022, 2:30 p.m."]
     },
     methods : {
         changePage : (i) => {
