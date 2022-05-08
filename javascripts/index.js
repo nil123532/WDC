83b1193866242248829f5b7124381fc38f7a8937
Patrick Sumarli <70998845+PatrickS136@users.noreply.github.com>
2022-05-08 06:03:03 +0000

    events page

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,7 +2,29 @@ var vueinst = new Vue({
     el : "#app",
     data : {
         currentPage : -1,
-        pages : [{name : "Home", link : "#"}, {name : "Events", link : "#"}, {name : "Settings", link : "#"}]
+        pages : [{name : "Home", link : "#"}, {name : "Events", link : "#"}, {name : "Settings", link : "#"}],
+        events : [{
+            name : "Pizza party",
+            desc : "At dominos",
+            date : "xd",
+            time : "xd",
+            count : 100,
+            img : "https://www.w3schools.com/w3css/img_lights.jpg"
+        },{
+            name : "Pizza party",
+            desc : "At dominos",
+            date : "xd",
+            time : "xd",
+            count : 100,
+            img : "https://www.w3schools.com/w3css/img_lights.jpg"
+        },{
+            name : "Pizza party",
+            desc : "At dominos",
+            date : "xd",
+            time : "xd",
+            count : 100,
+            img : "https://www.w3schools.com/w3css/img_lights.jpg"
+        },]
     },
     methods : {
         changePage : (i) => {
