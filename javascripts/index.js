c64f1e2fdde20c19e1f52cdb2a45e0d750220be7
nil123532 <66903808+nil123532@users.noreply.github.com>
2022-05-11 00:26:56 +0930

    New images and relevant vue script

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,25 +9,60 @@ var vueinst = new Vue({
             date : "xd",
             time : "xd",
             count : 100,
-            img : "https://www.w3schools.com/w3css/img_lights.jpg"
+            address : "Address1",
+            created :"",
+            confirmed:false
         },{
             name : "Pizza party",
             desc : "At dominos",
             date : "xd",
             time : "xd",
             count : 100,
-            img : "https://www.w3schools.com/w3css/img_lights.jpg"
+            address : "Address2",
+            created:"Created by Dominic Fike",
+            confirmed:true
         },{
             name : "Pizza party",
             desc : "At dominos",
             date : "xd",
             time : "xd",
             count : 100,
-            img : "https://www.w3schools.com/w3css/img_lights.jpg"
+            address : "Address3",
+            created:"",
+            confirmed:false
+        },],
+        adminevents : [{
+            name : "Pizza party",
+            desc : "At dominos",
+            date : "xd",
+            time : "xd",
+            count : 100,
+            address : "Address1",
+            created :"Created by Dominic Fike",
+            confirmed:false
+        },{
+            name : "Pizza party",
+            desc : "At dominos",
+            date : "xd",
+            time : "xd",
+            count : 100,
+            address : "Address2",
+            created:"Created by Dominic Fike",
+            confirmed:true
+        },{
+            name : "Pizza party",
+            desc : "At dominos",
+            date : "xd",
+            time : "xd",
+            count : 100,
+            address : "Address3",
+            created:"Created by Dominic Fike",
+            confirmed:false
         },],
         dummyDates : ["December 9th 2022", "December 13th 2022", "December 13th 2022"],
         times : ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"],
-        possibleTimes : ["December 9th 2022, 9PM", "December 13th 2022, 3AM", "December 13th 2022, 4PM"]
+        possibleTimes : ["December 9th 2022, 9PM", "December 13th 2022, 3AM", "December 13th 2022, 4PM"],
+        users:["Julia Robbins","Samuel Smith"]
     },
     methods : {
         changePage : (i) => {
