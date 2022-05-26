efcea66c89cdc062f3d271510bb6202a8b7b3d2c
Patrick Sumarli <70998845+PatrickS136@users.noreply.github.com>
2022-05-26 10:01:44 +0000

    Database setup

---
+++ meetups.sql
@@ -0,0 +1,148 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: meetups
+-- ------------------------------------------------------
+-- Server version	8.0.29-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `meetups`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `meetups` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `meetups`;
+
+--
+-- Table structure for table `Availability`
+--
+
+DROP TABLE IF EXISTS `Availability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Availability` (
+  `startTime` datetime NOT NULL,
+  `event_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  KEY `event_id` (`event_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `Availability_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`),
+  CONSTRAINT `Availability_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Availability`
+--
+
+LOCK TABLES `Availability` WRITE;
+/*!40000 ALTER TABLE `Availability` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Availability` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Dates`
+--
+
+DROP TABLE IF EXISTS `Dates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Dates` (
+  `event_id` int NOT NULL,
+  `date` date NOT NULL,
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `Dates_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Dates`
+--
+
+LOCK TABLES `Dates` WRITE;
+/*!40000 ALTER TABLE `Dates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Dates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Event`
+--
+
+DROP TABLE IF EXISTS `Event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Event` (
+  `event_id` int NOT NULL AUTO_INCREMENT,
+  `creator_id` int DEFAULT NULL,
+  `address_street_name` varchar(50) DEFAULT NULL,
+  `address_street_number` varchar(10) DEFAULT NULL,
+  `address_state` varchar(50) DEFAULT NULL,
+  `address_city` varchar(50) DEFAULT NULL,
+  `address_postcode` varchar(10) DEFAULT NULL,
+  `address_country` varchar(50) DEFAULT NULL,
+  `finalised_time` datetime DEFAULT NULL,
+  `duration` int unsigned DEFAULT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`event_id`),
+  KEY `creator_id` (`creator_id`),
+  CONSTRAINT `Event_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `User` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Event`
+--
+
+LOCK TABLES `Event` WRITE;
+/*!40000 ALTER TABLE `Event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `google` varchar(100) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-26 10:00:28
