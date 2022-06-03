c22c0698c5b1194f8f5f7cf54fc4838153d0821c
Patrick Sumarli <70998845+PatrickS136@users.noreply.github.com>
2022-06-03 19:47:08 +0000

    Fri, Jun 3, 2022, 7:47 PM +00:00

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,26 @@ router.get('/get_events', function(req, res, next){
   });
 });
 
+// GET details for an event
+router.get("/get_event_details/:eventid", function(req, res, next){
+  req.pool.getConnection(function(err, connection){
+    if (err){
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+    var query = "SELECT * FROM Event WHERE event_id=?;";
+    connection.query(query, [req.params.eventid], function(err2, rows, fields){
+      connection.release();
+      if (err2){
+        console.log("SQL Error");
+        console.log(query);
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows);
+    });
+  });
+});
+
 module.exports = router;
