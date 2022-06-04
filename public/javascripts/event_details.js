01f6330a83af4fe42bc333a044d3b9893799d7b5
Patrick Sumarli <70998845+PatrickS136@users.noreply.github.com>
2022-06-04 01:10:56 +0000

    Sat, Jun 4, 2022, 1:10 AM +00:00

---
+++ public/javascripts/event_details.js
@@ -0,0 +1,31 @@
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+var vueinst = new Vue({
+    el : "#app",
+    data : {
+        currentPage : -1,
+        pages : [{name : "Home", link : "/home.html"}, {name : "Events", link : "/events.html"}, {name : "Settings", link : "/settings.html"}],
+        eventDetail : {},
+        dummyDates : ["December 9th 2022", "December 13th 2022", "December 13th 2022"],
+        times : ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"],
+        possibleTimes : ["December 9th 2022, 9PM", "December 13th 2022, 3AM", "December 13th 2022, 4PM"],
+        users:["Julia Robbins","Samuel Smith"]
+    },
+    methods : {
+        changePage : (i) => {
+            this.currentPage = 2;
+        },
+    },
+    mounted : function(){
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if (this.readyState==4 && this.status == 200){
+                vueinst.eventDetail = JSON.parse(this.responseText)[0];
+            }
+        };
+        xhttp.open("GET", "/get_event_details/" + params.eventid, true);
+        xhttp.send();
+    },
+});
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -43,17 +43,4 @@ var vueinst = new Vue({
             this.currentPage = 2;
         },
     },
-    // mounted : function(){
-    //     const xhttp = new XMLHttpRequest();
-    //     xhttp.onreadystatechange = function(){
-    //         if (this.readyState==4 && this.status == 200){
-    //             for (const i of JSON.parse(this.responseText)){
-    //                 console.log(i);
-    //                 vueinst.events.push(i);
-    //             }
-    //         }
-    //     };
-    //     xhttp.open("GET", "/get_events", true);
-    //     xhttp.send();
-    // },
 });
