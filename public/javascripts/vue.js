96b46b4cbf9f7657193b42a1327ff05c2193f523
Somebodyboy <ubuntu@LEGION.localdomain>
2022-06-05 19:33:32 +0800

    Fixing accidental broken logout

--- public/javascripts/vue.js
+++ public/javascripts/vue.js
@@ -18,7 +18,7 @@ var vueinst = new Vue
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    location.href = '/user-sign-in.html';
+                    location.href = '/index.html';
                 }
              };
             xhttp.open("GET", "/users/logout", true);
