640cad9ac74dbb7086a139b0bf67a3b2370da330
sharjeel17 <71211880+sharjeel17@users.noreply.github.com>
2022-06-06 19:32:05 +0100

    Settings page update

--- public/javascripts/vue.js
+++ public/javascripts/vue.js
@@ -7,6 +7,10 @@ var vueinst = new Vue
         last_name : "",
         email :  "",
         password : "",
+        new_password : "",
+        confirm_password : "",
+        passwordError : "",
+        passError : false,
         pages : [{name : "Home", link : "/home"}, {name : "Events", link : "/events"}, {name : "Settings", link : "/settings"}],
     },
     methods:
@@ -24,6 +28,41 @@ var vueinst = new Vue
              };
             xhttp.open("GET", "/users/logout", true);
             xhttp.send();
+        },
+
+        passwordChange1:function(){
+
+            if(!(this.new_password == "") && this.confirm_password == ""){
+                this.passError = true;
+                this.passwordError = "Enter new password in confirm password";
+                return;
+            }
+            else if(this.new_password != this.confirm_password && !(this.new_password == "" && this.confirm_password == "")){
+                this.passError = true;
+                this.passwordError = "Passwords do not match";
+                return;
+            }
+            else if(this.new_password == this.confirm_password && !(this.new_password == "" && this.confirm_password == "")){
+                var xhttp = new XMLHttpRequest();
+
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+
+                        var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if (this.readyState == 4 && this.status == 200) {
+                                location.href = '/';
+                            }
+                        };
+                        xhttp.open("GET", "/users/logout", true);
+                        xhttp.send();
+                    }
+                 };
+
+                xhttp.open("POST", "/settingsChangePassword1", true);
+                xhttp.setRequestHeader("Content-type", "application/json");
+                xhttp.send(JSON.stringify({new_password: this.new_password }));
+            }
         }
     }
 });
@@ -33,4 +72,21 @@ function GsignOut() {
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
+}
+
+
+///FUNCTIONS FOR SETTINGS PAGE
+function getSettingsInfo(){
+    var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                let rows = JSON.parse(this.responseText);
+                document.getElementById("settingsFirstName").setAttribute("value",rows[0].first_name);
+                document.getElementById("settingsLastName").setAttribute("value",rows[0].last_name);
+                document.getElementById("settingsEmail").setAttribute("value",rows[0].email);
+
+            }
+        };
+        xhttp.open("GET", "/users/getSettingsInfo", true);
+        xhttp.send();
 }
