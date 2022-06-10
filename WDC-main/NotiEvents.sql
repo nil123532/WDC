732eac491205a74f40a662e7eb8adcb916f6f4b3
nil123532 <66903808+nil123532@users.noreply.github.com>
2022-06-10 17:56:47 +0930

    Delete WDC-folder

--- WDC-main/NotiEvents.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Notifications (
-    NotiCancel boolean,
-    NotiRespond boolean,
-    NotiFinal boolean,
-    user_id int,
-    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE
-);
